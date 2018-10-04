dba48c34812e6ca290fbfaecc0962f4727003c84
Newman Thurairatnam <newman.joyal@gmail.com>
2018-10-04 12:55:53 -0400

    Slight refactoring

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,26 +1,3 @@
-from copy import deepcopy
-
-
-# Quicksort
-# [5 1 7 4 2 3] pivot 5
-# [1 4 2 3] 5 [7]
-
-# Quicksort 2
-# 1 4 2 3
-# [] 1 [4 2 3]
-# [7] return 7
-
-# Quicksort 3
-#  [4 2 3]
-# [2 3] 4 []
-
-# Quicksort 4
-# 3 2 4
-# 2 3 4
-
-# Quicksort 5
-#
-
 def partition(list_numbers):
     first = list_numbers[0]
     lower = []
